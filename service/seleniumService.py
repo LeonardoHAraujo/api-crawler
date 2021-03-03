17ae08d2b4ef7356c52099e4f23a6e4cd7fcb0d4
Leonardo <leonardo.silva@privally.global>
2021-03-03 16:23:07 -0300

    refactoring in seleniumService.py

--- service/seleniumService.py
+++ service/seleniumService.py
@@ -19,55 +19,68 @@ class SeleniumService:
     def search(self, search):
         """ Bunge function on the site all tasty (capture of the first 3 recipes) """
 
-        self.driver.get(f'https://www.tudogostoso.com.br/busca?q={search}')
-        sleep(1)
+        try:
+            self.driver.get(f'https://www.tudogostoso.com.br/busca?q={search}')
+            sleep(1)
 
-        # RECIPES CAPTURED
-        recipe_1 = self.driver.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[1]')
-        recipe_2 = self.driver.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[3]')
-        recipe_3 = self.driver.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[4]')
+            # RECIPES CAPTURED
+            recipe_1 = self.driver.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[1]')
+            recipe_2 = self.driver.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[3]')
+            recipe_3 = self.driver.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[4]')
 
-        # TITLE AND LINKS OF RECIPES
-        title_recipe_1 = recipe_1.find_element_by_class_name('title').text
-        a_recipe_1 = recipe_1.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[1]/a')
-        link_recipe_1 = a_recipe_1.get_attribute('href')
-        img_recipe_1 = recipe_1.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[1]/a/picture/img')
-        avatar_recipe_1 = img_recipe_1.get_attribute('src')
+            # TITLE AND LINKS OF RECIPES
+            title_recipe_1 = recipe_1.find_element_by_class_name('title').text
+            a_recipe_1 = recipe_1.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[1]/a')
+            link_recipe_1 = a_recipe_1.get_attribute('href')
+            img_recipe_1 = recipe_1.find_element_by_xpath(
+                '/html/body/div[5]/div[1]/div[2]/div[2]/div/div[1]/a/picture/img')
+            avatar_recipe_1 = img_recipe_1.get_attribute('src')
 
-        title_recipe_2 = recipe_2.find_element_by_class_name('title').text
-        a_recipe_2 = recipe_2.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[1]/a')
-        link_recipe_2 = a_recipe_2.get_attribute('href')
-        img_recipe_2 = recipe_2.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[3]/a/picture/img')
-        avatar_recipe_2 = img_recipe_2.get_attribute('src')
+            title_recipe_2 = recipe_2.find_element_by_class_name('title').text
+            a_recipe_2 = recipe_2.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[1]/a')
+            link_recipe_2 = a_recipe_2.get_attribute('href')
+            img_recipe_2 = recipe_2.find_element_by_xpath(
+                '/html/body/div[5]/div[1]/div[2]/div[2]/div/div[3]/a/picture/img')
+            avatar_recipe_2 = img_recipe_2.get_attribute('src')
 
-        title_recipe_3 = recipe_3.find_element_by_class_name('title').text
-        a_recipe_3 = recipe_3.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[1]/a')
-        link_recipe_3 = a_recipe_3.get_attribute('href')
-        img_recipe_3 = recipe_3.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[4]/a/picture/img')
-        avatar_recipe_3 = img_recipe_3.get_attribute('src')
+            title_recipe_3 = recipe_3.find_element_by_class_name('title').text
+            a_recipe_3 = recipe_3.find_element_by_xpath('/html/body/div[5]/div[1]/div[2]/div[2]/div/div[1]/a')
+            link_recipe_3 = a_recipe_3.get_attribute('href')
+            img_recipe_3 = recipe_3.find_element_by_xpath(
+                '/html/body/div[5]/div[1]/div[2]/div[2]/div/div[4]/a/picture/img')
+            avatar_recipe_3 = img_recipe_3.get_attribute('src')
 
-        recipes = [
-            {
-                'Title': title_recipe_1,
-                'Link': link_recipe_1,
-                'Avatar': avatar_recipe_1
-            },
-            {
-                'Title': title_recipe_2,
-                'Link': link_recipe_2,
-                'Avatar': avatar_recipe_2
-            },
-            {
-                'Title': title_recipe_3,
-                'Link': link_recipe_3,
-                'Avatar': avatar_recipe_3
-            },
-        ]
+            recipes = [
+                {
+                    'Title': title_recipe_1,
+                    'Link': link_recipe_1,
+                    'Avatar': avatar_recipe_1
+                },
+                {
+                    'Title': title_recipe_2,
+                    'Link': link_recipe_2,
+                    'Avatar': avatar_recipe_2
+                },
+                {
+                    'Title': title_recipe_3,
+                    'Link': link_recipe_3,
+                    'Avatar': avatar_recipe_3
+                },
+            ]
 
-        resp = {
-            'Status': 200,
-            'Message': 'Essas são as três primeiras receitas de Tudo Gostoso',
-            'Receitas': recipes
-        }
+            resp = {
+                'Status': 200,
+                'Message': 'Essas são as três primeiras receitas de Tudo Gostoso',
+                'Receitas': recipes
+            }
 
-        return json.dumps(resp)
+            return json.dumps(resp)
+
+        except:
+
+            resp_fail = {
+                'Status': 400,
+                'Message': 'Receita não encontrada'
+            }
+
+            return json.dumps(resp_fail)
